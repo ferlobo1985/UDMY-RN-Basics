4f313c5d4d62aa5c8f2da269ed587e3f758ad82c
Coding rev <fernando.lobo.prez@gmail.com>
2024-10-30 06:53:20 -0300

    13-Dimension API
    13-Dimension API

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image,ImageBackground } from 'react-native';
-import { useState } from 'react';
-
-import Wave from './assets/images/Wave.jpg'
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
 /// COMPONENTS
 import Nav from './components/nav';
-import ModalComponent from './components/modal';
 
 export default function App() {
+
+
   return (
       <View>
           <Nav nameOfApp="Awesome App"/>
-          <ModalComponent/>
+          <View style={{flexDirection:'column',alignItems:'center'}}>
+            <View style={styles.box}>
+                <Text>One</Text>
+            </View>
+          </View>
       </View>
   );
 }
 
+const width = Dimensions.get('window').width;
+const height = Dimensions.get('window').height;
+
 const styles = StyleSheet.create({
+  box:{
+    width: width < 400 ? 200:250,
+    height: width < 400 ? 200:250,
+    margin: width > 350 ? 10:30,
+    padding:10,
+    alignItems:'center',
+    justifyContent:'center',
+    backgroundColor: width > 400 ? 'yellow':'lightblue'
+  }
 })
