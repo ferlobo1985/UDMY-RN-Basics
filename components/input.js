52d1ec267b2fb5323c5eb44a481b96f45502e130
Coding rev <fernando.lobo.prez@gmail.com>
2024-10-30 04:20:14 -0300

    08-Button
    08-Button

--- components/input.js
+++ components/input.js
@@ -1,26 +1,42 @@
 import { useState } from "react";
-import { Text, View, StyleSheet, TextInput } from "react-native";
+import { Text, View, StyleSheet, TextInput, Button, ScrollView } from "react-native";
 
 export default function InputComp(){
-    const [myInput,setMyInput] = useState('')
+    const [myInput,setMyInput] = useState('');
+    const [names,setNames] = useState(['John','James','Mark','Jane','Lisa','Mike','Miles','Millie','Mikel'])
 
     const onChangeInput = (event) => {
         setMyInput(event)
     }
 
+    const onAddUser = () => {
+        const newState = [...names,myInput];
+        setNames(newState);
+        setMyInput('');
+    }
+
     return(
-        <View>
-            <TextInput
-                value={myInput}
-                style={styles.input}
-                onChangeText={(e)=>onChangeInput(e)}
-                multiline={true}
-                maxLength={5}
-                // editable={false}
-                placeholder="Enter your name"
-                keyboardType="number-pad"
+        <ScrollView>
+            <View>
+                <TextInput
+                    value={myInput}
+                    style={styles.input}
+                    onChangeText={(e)=>onChangeInput(e)}
+                    placeholder="Enter your name"
+                />
+            </View>
+            <Button
+                title="Add user"
+                onPress={onAddUser}
             />
-        </View>
+            <View style={{marginTop:10}}>
+                { names.map((name,index)=>(
+                    <Text key={index} style={styles.user}>
+                        {name}
+                    </Text>
+                ))}
+            </View>
+        </ScrollView>
     )
 }
 
@@ -31,5 +47,12 @@ const styles =  StyleSheet.create({
         backgroundColor:'#f2f2f2',
         padding:10,
         fontSize:20
+    },
+    user:{
+        fontSize:40,
+        borderWidth:1,
+        borderColor:'#cecece',
+        padding:10,
+        marginBottom:20
     }
 })
