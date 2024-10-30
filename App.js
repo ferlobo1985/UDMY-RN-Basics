7e522c65e2da097317530423d8c4b2ce395b7fbe
Coding rev <fernando.lobo.prez@gmail.com>
2024-10-30 07:11:36 -0300

    15-Use Window dimension
    15-Use Window dimension

--- App.js
+++ App.js
@@ -1,17 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, useWindowDimensions } from 'react-native';
 
 /// COMPONENTS
 import Nav from './components/nav';
 
 export default function App() {
+  const { width, height} = useWindowDimensions();
+  console.log(width, height)
 
+  let boxSize = height > 400 ? 200:400;
+  let color = height > 400 ? { backgroundColor:'yellow'}:{backgroundColor:'lightblue'}
 
   return (
       <View>
           <Nav nameOfApp="Awesome App"/>
           <View style={{flexDirection:'column',alignItems:'center'}}>
-            <View style={styles.box}>
+            <View style={[styles.box,{
+              width:boxSize,
+              height:boxSize
+            },
+            color
+            ]}>
                 <Text>One</Text>
             </View>
           </View>
@@ -24,12 +33,12 @@ const height = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
   box:{
-    width: width < 400 ? 200:250,
-    height: width < 400 ? 200:250,
+    // width: width < 400 ? 200:250,
+    // height: width < 400 ? 200:250,
     margin: width > 350 ? 10:30,
     padding:10,
     alignItems:'center',
     justifyContent:'center',
-    backgroundColor: width > 400 ? 'yellow':'lightblue'
+    // backgroundColor: width > 400 ? 'yellow':'lightblue'
   }
 })
